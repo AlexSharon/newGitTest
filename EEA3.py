ca06dba764c47485cd48fe5e4b45ac4110a62e22
Alexey Sharonov <sharonov-azlk@mail.ru>
2017-09-25 08:27:10 +0000

    short

--- EEA3.py
+++ EEA3.py
@@ -1,10 +1,3 @@
-def gcd(a, b):
-    """returns gcd of two values"""
-    while(a != 0 and b != 0):
-        if(a >= b):
-            a = a % b
-        else:
-            b = b % a
-    return a + b
+
 
 #added line
