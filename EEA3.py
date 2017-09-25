b476f5dcf6f3c370841a622f4d96bee8b17b1e01
Alexey Sharonov <sharonov-azlk@mail.ru>
2017-09-25 08:36:07 +0000

    2

--- EEA3.py
+++ EEA3.py
@@ -1,3 +1,10 @@
-
+def gcd(a, b):
+    """returns"""
+    while(a != 0 and b != 0):
+        if(a >= b):
+            a = a % b
+        else:
+            b = b % a
+    return a + b
 
 #added line
