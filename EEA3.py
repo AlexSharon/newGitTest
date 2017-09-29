ba1447dfebdf3fbe23a3af2b76df3e64b9272f15
Alexey Sharonov <sharonov-azlk@mail.ru>
2017-09-29 07:18:52 +0000

    change to both files

--- EEA3.py
+++ EEA3.py
@@ -1,29 +1,2 @@
-def gcd(a, b):
-    """returns"""
-    while(a != 0 and b != 0):
-        if(a >= b):
-            a = a % b
-        else:
-            b = b % a
-    return a + b
-
-"""new comment"""
-#added line
-
-def GGG(a, b):
-    """returns gcd of two values"""
-    while(a != 0 and b != 0):
-        if(a >= b):
-            a = a % b
-        else:
-            b = b % a
-    return a + b
-
-def test(a, b):
-    """returns"""
-
-#other
-
-print("foo")
 
 print("boo01222")
